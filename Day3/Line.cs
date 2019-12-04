f29b39f7d0734406a977a56aabdf2eeb7e0ac2f8
David Betteridge <david.betteridge@proactis.com>
2019-12-04 19:09:44 +0000

    Day 3 Part 1

---
+++ Day3/Line.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Day3
+{
+    internal class Line
+    {
+        private (int x, int y) point1;
+        private (int x, int y) point2;
+
+        public Line((int x, int y) point1, (int x, int y) point2)
+        {
+            this.point1 = (Math.Min(point1.x, point2.x), Math.Min(point1.y, point2.y));
+            this.point2 = (Math.Max(point1.x, point2.x), Math.Max(point1.y, point2.y));
+        }
+
+        public bool IsVertical => point1.x == point2.x;
+        public bool IsHorizontal => point1.y == point2.y;
+
+        public int Distance => Math.Min(Math.Abs(point1.x + point1.y), Math.Abs(point2.x + point2.y));
+
+        public bool DoLinesCross(Line otherLine, out (int x, int y) point)
+        {
+            point = (0, 0);
+
+            if (this.IsHorizontal == otherLine.IsHorizontal) return false;
+
+            var y = this.IsHorizontal ? this.point1.y : otherLine.point1.y;
+            var x = this.IsVertical ? this.point1.x : otherLine.point1.x;
+
+            if (x >= this.point1.x && x >= otherLine.point1.x && x <= this.point2.x && x <= otherLine.point2.x &&
+                y >= this.point1.y && y >= otherLine.point1.y && y <= this.point2.y && y <= otherLine.point2.y)
+            {
+                point = (x, y);
+                return true;
+            }
+
+            return false;
+        }
+
+
+        /*
+         *  Line1,      0,0  ->  0,10   x=0
+         *  Line2,      0,0  -> 10, 0  y=10  
+         * x
+         * x
+         * xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+         * x
+         * x
+         * x
+         * x
+         * x
+         * x
+         * x
+         * x
+         * x
+         * 
+         * 
+         */
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main()
+        {
+            var lines = File.ReadAllLines("Part1.txt");
+            var route1 = BuildLines(lines[0].Split(','));
+            var route2 = BuildLines(lines[1].Split(','));
+
+            var intersections = new List<(int x, int y)>();
+            foreach (var line in route1)
+            {
+                foreach (var line2 in route2)
+                {
+                    if (line.DoLinesCross(line2, out var intersection))
+                    {
+                        intersections.Add(intersection);
+                    }
+                }
+            }
+
+            var distances = intersections
+                                .Select(pt => Math.Abs(pt.x) + Math.Abs(pt.y))
+                                .OrderBy(d => d);
+        }
+
+        private static List<Line> BuildLines(string[] moves)
+        {
+            var lines = new List<Line>();
+
+            (int x, int y) point = (0, 0);
+            foreach (var move in moves)
+            {
+                var direction = move[0];
+                var distance = int.Parse(move.Substring(1));
+
+                var newPoint = direction switch
+                {
+                    'R' => (point.x + distance, point.y),
+                    'L' => (point.x - distance, point.y),
+                    'U' => (point.x, point.y + distance),
+                    _ => (point.x, point.y - distance),
+                };
+
+                lines.Add(new Line(point, newPoint));
+
+                point = newPoint;
+            }
+
+            return lines;
+        }
+    }
+}
