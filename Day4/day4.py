b324eb54717068de27bf7d92b9121c062bbcdb12
David Betteridge <david.betteridge@proactis.com>
2020-12-08 23:26:35 +0000

    Add unit tests

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,34 +1,26 @@
-lower = 178416
-upper = 676461
+LOWER = 178416
+UPPER = 676461
 
-def is_valid_for_part_one(number):
-    if not (lower <= number <= upper):
-        return False
+def is_valid_for_part_one(password):
+    return any(password[i] == password[i + 1] for i in range(0, 5))
 
-    as_text = str(number)
-    for i in range(0, len(as_text) - 1):
-        if as_text[i] == as_text[i+ 1] : return True
+def is_valid_for_part_two(password):
+    return any(password[i] == password[i + 1] and password.count(password[i]) == 2 for i in range(0, 5))
 
-    return False
+def possible_passwords():
+    for n1 in range(1, 7):
+        for n2 in range(n1, 10):
+            for n3 in range(n2, 10):
+                for n4 in range(n3, 10):
+                    for n5 in range(n4, 10):
+                        for n6 in range(n5, 10):
+                            number = f'{n1}{n2}{n3}{n4}{n5}{n6}'
+                            if (LOWER <= int(number) <= UPPER):
+                                yield number
 
-def is_valid_for_part_two(number):
-    if not (lower <= number <= upper):
-        return False
+def part_one():
+    return sum([is_valid_for_part_one(x) for x in possible_passwords()])
 
-    as_text = str(number)
-    for i in range(0, len(as_text) - 1):
-        if as_text[i] == as_text[i+ 1] and as_text.count(as_text[i]) == 2 : return True
+def part_two():
+    return sum([is_valid_for_part_two(x) for x in possible_passwords()])
 
-    return False
-
-good_passwords = 0
-for n1 in range(1, 7):
-    for n2 in range(n1, 10):
-        for n3 in range(n2, 10):
-            for n4 in range(n3, 10):
-                for n5 in range(n4, 10):
-                    for n6 in range(n5, 10):
-                        toTry = int(f'{n1}{n2}{n3}{n4}{n5}{n6}')
-                        if is_valid_for_part_two(toTry):
-                            good_passwords += 1
-print(good_passwords)                            
