9de344f788efea34f88f18143c52d5762e03f04f
David Betteridge <david.betteridge@proactis.com>
2020-12-08 23:10:39 +0000

    Day 4 - Part 2

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,13 +1,23 @@
 lower = 178416
 upper = 676461
 
-def is_valid(number):
+def is_valid_for_part_one(number):
     if not (lower <= number <= upper):
         return False
 
     as_text = str(number)
     for i in range(0, len(as_text) - 1):
         if as_text[i] == as_text[i+ 1] : return True
+
+    return False
+
+def is_valid_for_part_two(number):
+    if not (lower <= number <= upper):
+        return False
+
+    as_text = str(number)
+    for i in range(0, len(as_text) - 1):
+        if as_text[i] == as_text[i+ 1] and as_text.count(as_text[i]) == 2 : return True
 
     return False
 
@@ -19,6 +29,6 @@ for n1 in range(1, 7):
                 for n5 in range(n4, 10):
                     for n6 in range(n5, 10):
                         toTry = int(f'{n1}{n2}{n3}{n4}{n5}{n6}')
-                        if is_valid(toTry):
+                        if is_valid_for_part_two(toTry):
                             good_passwords += 1
 print(good_passwords)                            
